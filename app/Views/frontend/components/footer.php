223ba14481125975c1a22ae79e0592ab1df69ff7
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-07-21 23:45:35 +0700

    [landing page]: fix footer, fix font size and font color, create tooltip whatsapp

--- app/Views/frontend/components/footer.php
+++ app/Views/frontend/components/footer.php
@@ -1,28 +1,29 @@
 <footer class="bg-[#8068B3]">
-    <header class="font-bold">
-        <h1>Hubungi Kami</h1>
-    </header>
-    <article class="grid grid-cols-2 gap-4">
-        <section>
-            <header class="font-bold">
-                <h2>TK / TKIT Muslimat NU</h2>
-            </header>
-            <p>3J84+5RF, Jl. Cemp., Tengah, Babalanlor, Kec. Bojong, Kabupaten Pekalongan, Jawa Tengah 51156</p>
-        </section>
-        <section>
+    <article class="container px-24 py-16 mx-auto">
+        <header class="font-bold text-white">
+            <h1 class="text-2xl">Hubungi Kami</h1>
+        </header>
+        <section class="grid grid-cols-2 gap-4 mt-4">
             <article>
-                <header class="font-bold">
-                    <h2>WhatsApp Staff TU (Pak Fulan)</h2>
+                <header class="font-bold text-white">
+                    <h2>TK / TKIT Muslimat NU</h2>
                 </header>
-                <p>+62 812-2918-2019</p>
+                <p class="text-white">3J84+5RF, Jl. Cemp., Tengah, Babalanlor, Kec. Bojong, Kabupaten Pekalongan, Jawa Tengah 51156</p>
             </article>
-            <article>
-                <header class="font-bold">
-                    <h2>Email TU Sekolah</h2>
-                </header>
-                <p>tkmuslimatnubabalanlor@gmail.com</p>
-            </article>
+            <section class="flex flex-col gap-5">
+                <article>
+                    <header class="font-bold text-white">
+                        <h2>WhatsApp Staff TU (Pak Fulan)</h2>
+                    </header>
+                    <p class="text-white">+62 812-2918-2019</p>
+                </article>
+                <article>
+                    <header class="font-bold text-white">
+                        <h2>Email TU Sekolah</h2>
+                    </header>
+                    <p class="text-white">tkmuslimatnubabalanlor@gmail.com</p>
+                </article>
+            </section>
         </section>
     </article>
-
 </footer>
