562238e1d14b1bdb8c4dc5cfda1c28bc7b1d5bc8
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-07-22 23:12:57 +0700

    [administrator][feat]: routing untuk login, dashboard, pengumuman, tahun akademik, kelas, siswa

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,10 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'Administrator\Auth\Login::index');
+$routes->get('/dashboard', 'Administrator\Dashboard\Dashboard::index');
+$routes->get('/dashboard/pengumuman', 'Administrator\Pengumuman\Pengumuman::index');
+$routes->get('/dashboard/tahun-akademik', 'Administrator\Tahun_Akademik\TahunAkademik::index');
+$routes->get('/dashboard/kelas', 'Administrator\Kelas\Kelas::index');
+$routes->get('/dashboard/siswa', 'Administrator\Siswa\Siswa::index');
+$routes->get('/dashboard/siswa/create', 'Administrator\Siswa\Siswa::create');
