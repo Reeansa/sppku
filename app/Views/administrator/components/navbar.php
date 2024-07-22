256445d227bc3b55de7bb6f474ea5fadd9f1a7dc
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-07-22 23:17:07 +0700

    [administrator][feat]: navigasi bar

---
+++ app/Views/administrator/components/navbar.php
@@ -0,0 +1,15 @@
+<nav class="fixed flex items-center justify-between w-full px-10 pl-64 shadow-default bg-[#F0F5F1] top-0 h-20">
+    <div class="flex items-center gap-5 px-10">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <path d="M3 18V16H21V18H3ZM3 13V11H21V13H3ZM3 8V6H21V8H3Z" fill="#3D4946" />
+        </svg>
+        <h1 class="text-[#005B4D]">Dashboard</h1>
+    </div>
+    <div class="text-sm breadcrumbs">
+        <ul>
+            <li><a>Home</a></li>
+            <li><a>Documents</a></li>
+            <li>Add Document</li>
+        </ul>
+    </div>
+</nav>
