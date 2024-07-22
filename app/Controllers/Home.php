8734d48c95b94c23865c1e16406dfc531264c780
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-07-22 23:14:00 +0700

    [landing-page][refactor]: perubahaan pemanggilan view

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('frontend/home/index');
+        return view('frontend/pages/home/index');
     }
 }
